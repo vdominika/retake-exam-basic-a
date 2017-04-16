85e4d2f8a6d972fce0a4f3465c4354d963e0f2c3
gy0p4k <gyorgypakozdi@gmail.com>
2017-04-16 21:36:16 +0200

    Update MostCommonWord.java

--- most-common-word/MostCommonWord.java
+++ most-common-word/MostCommonWord.java
@@ -2,10 +2,10 @@ public class MostCommonWord {
   public static void main(String[] args) {
     // This should be a simple word counter which give us the most common string in a file
     // If ran from the command line without arguments it should print out the usage:
-    // java MostCommonWord [source] [word]
-    // When just one argument is provided print out
-    // No source or word provided
-    // When both arguments provided and the source is a file
+    // java MostCommonWord [source]
+    // When no argument is provided print out
+    // No source provided
+    // When the argument provided and the source is a file
     // count all words in the given file and print the most common 
   }
 }
