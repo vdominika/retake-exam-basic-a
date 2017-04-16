c46a406aa61fb570e1a8dfa7f33c9899d22d0938
gy0p4k <gyorgypakozdi@gmail.com>
2017-04-16 21:37:09 +0200

    Update mostcommonword.py

--- most-common-word/mostcommonword.py
+++ most-common-word/mostcommonword.py
@@ -1,7 +1,7 @@
 # This should be a simple word counter which give us the most common word in a file
 # If ran from the command line without arguments it should print out the usage:
-# mostcommonword [source] [word]
-# When just one argument is provided print out
-# No source or word provided
-# When both arguments provided and the source is a file
+# mostcommonword [source]
+# When no argument is provided print out
+# No source provided
+# When the argument provided and the source is a file
 # count all words in the given file and print the most common 
